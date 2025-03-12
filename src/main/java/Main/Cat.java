afe2bd5710df4ff9933b03b8756be1a8e747ffdf
aeen bolandian <bolandian85@gmail.com>
2025-03-12 09:46:00 +0330

    gender , age , name and weight added to mammal class + getter and setters + constructor

--- src/main/java/Main/Cat.java
+++ src/main/java/Main/Cat.java
@@ -1,5 +1,4 @@
 package Main;
 
 public class Cat extends Mammal implements Carnivore {
-
 }
--- src/main/java/Main/Mammal.java
+++ src/main/java/Main/Mammal.java
@@ -1,6 +1,44 @@
 package Main;
 
 public abstract class Mammal implements Creature {
+    private String name;
+    private String gender;
+    private int age;
+    private int weight;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Mammal(String name, String gender , int age, int weight) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.weight = weight;
+    }
+
     public abstract void move();
     public abstract void makeSound();
     @Override
