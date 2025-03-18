f41491b5c048ea2ca79d166bf579adf454ffa7d2
MOOH <arad3.138563@gmail.com>
2025-03-19 00:54:28 +0330

    I am done with cats

--- src/main/java/Main/Cat.java
+++ src/main/java/Main/Cat.java
@@ -1,4 +1,8 @@
 package Main;
 
-public class Cat {
+class Cat extends Mammal implements Carnivore {
+    @Override
+    public void eatMeat() {
+        System.out.println("Cat is eating meat.");
+    }
 }
